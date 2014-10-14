b1601ae4e7e60d7fa61315cc5cea5466035ea5df
mattkiernan <matt.kiernan@gmail.com>
2014-10-14 16:32:37 -0400

    Add remaining files for displaying timeline shouts from partial

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -3,12 +3,8 @@ class ShoutsController < ApplicationController
   before_action :require_login
 
   def index
-    @shouts = Shout.all
     @shout = Shout.new
-    @users = User.all
-    @following_relationships = FollowingRelationship.new
-    @users_i_follow = current_user.followed_users 
-    @users_i_do_not_follow = User.where.not(id: current_user.followed_users)
+    @timeline = current_user.timeline
   end
 
   def create
