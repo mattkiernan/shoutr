376902e45fc20390c6cd058cf73d3f2e40052ae1
mattkiernan <matt.kiernan@gmail.com>
2014-10-16 16:44:14 -0400

    usernames mentioned in shouts link to user's page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,8 @@
 module ApplicationHelper
-  def bold(text)
-    # text.gsub(/@\w+/) do |username|
-    #   content_tag :strong, usernam
-    # end
-
-    username = /@\w+/.match(text)
-    boldname = content_tag(:strong, username)
-    text.gsub(/@\w+/, boldname)
+  def link(text)
+     text.gsub(/@(\w+)/) do |mention|
+       username = mention[1..-1]
+       link_to mention, user_path(username)
+     end
   end
 end
