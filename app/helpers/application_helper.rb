0c37ca32a83d7e3cf99fa8e2e38926864275298c
mattkiernan <matt.kiernan@gmail.com>
2014-10-17 09:40:19 -0400

    Enable hashtags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,20 @@ module ApplicationHelper
   def link(text)
      text.gsub(/@(\w+)/) do |mention|
        username = mention[1..-1]
-       link_to mention, user_path(username)
+       all_users = User.all
+       all_usernames = all_users.map{|user| user.username}
+       if all_usernames.include?(username)
+         link_to mention, user_path(username)
+       else
+         mention
+       end
      end
+  end
+
+  def hashtag(text)
+    text.gsub(/#(\w+)/) do |hash|
+      hashtag = hash[1..-1]
+      link_to hash, controller: "searches", query: hashtag, action: "show"
+    end
   end
 end
