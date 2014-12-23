cd16541fedfdf4ffbf232bc1fc2d886485463ed4
mattkiernan <matt.kiernan@gmail.com>
2014-12-23 18:15:08 -0500

    Shorten follower list, add Rails_12factor

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,7 @@
 class DashboardsController < ApplicationController
+  before_action :require_login
   def show
-    @users = User.all
+    @users = User.last(4)
     @text_subject = TextSubject.new
     @image_subject = ImageSubject.new
     @timeline = current_user.timeline.page(params[:page]).per(6)
